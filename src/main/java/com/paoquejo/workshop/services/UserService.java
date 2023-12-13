7801e798bca05bb0f82ed8f072e43bdb0998a028
EnzoPeixoto <enzopeixoto043@gmail.com>
2023-12-13 11:57:41 -0300

    Service layer, component registration

---
+++ src/main/java/com/paoquejo/workshop/services/UserService.java
@@ -0,0 +1,26 @@
+package com.paoquejo.workshop.services;
+
+import com.paoquejo.workshop.entities.User;
+import com.paoquejo.workshop.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
