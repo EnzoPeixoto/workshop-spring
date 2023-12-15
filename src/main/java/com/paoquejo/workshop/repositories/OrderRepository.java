4a46fc8984d3c9a6a8b282e368ddfe69f0d0a978
Enzo Peixoto <enzopeixoto043@gmail.com>
2023-12-15 13:01:33 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/paoquejo/workshop/repositories/OrderRepository.java
@@ -0,0 +1,8 @@
+package com.paoquejo.workshop.repositories;
+
+import com.paoquejo.workshop.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
